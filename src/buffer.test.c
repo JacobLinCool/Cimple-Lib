02829ca8744128304b340b44cf65400aea46e154
JacobLinCool <jacoblincool@gmail.com>
2022-05-03 19:10:14 +0800

    test: buffer.test memory cleanup

--- src/buffer.test.c
+++ src/buffer.test.c
@@ -23,18 +23,27 @@ int main() {
 
     Buffer.parse(buffer, str, 1024);
     char* str2 = Buffer.stringify(buffer, 1024, 8, 8);
+    assert(strcmp(str, str2) == 0);
+    free(str);
+    free(str2);
     // printf("%s\n", str2);
 
-    assert(*(uint32_t*)Buffer.reverse(&(uint32_t){0x01020304}, 4) == 0x04030201);
+    free(buffer);
+
+    uint32_t* reversed = Buffer.reverse(&(uint32_t){0x01020304}, 4);
+    assert(*reversed == 0x04030201);
+    free(reversed);
 
     size_t   size = 13;
     uint16_t val = 1234;
     // printf("%s\n", Buffer.stringify(&val, size, 8, 8));
     void* big = Buffer.to_big(&val, size);
     // printf("%s\n", Buffer.stringify(big, size, -1, 8));
-    uint16_t val_2 = *(uint16_t*)Buffer.to_little(big, size);
+    uint16_t* val_2 = Buffer.to_little(big, size);
     // printf("%s\n", Buffer.stringify(&val_2, size, 8, 8));
-    assert(val == val_2);
+    assert(val == *val_2);
+    free(big);
+    free(val_2);
 
     return EXIT_SUCCESS;
 }
